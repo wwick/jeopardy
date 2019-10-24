7b5f4d2fe0b6151c9324fa982463c2553a1d351c
wwick <wwick@wustl.edu>
2019-10-23 22:49:13 -0500

    commit

--- createHTML.js
+++ createHTML.js
@@ -17,16 +17,27 @@ function createTable() {
         $.each( data, function( _key, val)  {
             let question_text = val.question.replace(/<[^>]*>?/gm, '');
             let answer_text = val.answer.replace(/<[^>]*>?/gm, '');
-            let $row = $("<div>", {class:"row", id:val.id});
-            let $answer = $("<h1>", {class:"answer", text:answer_text});
-            let $question = $("<p>", {class:"question", text:question_text});
-            $row.append($answer);
-            $row.append($question);
+            let $row = $("<div>", {class:"clue", id:val.id});
+            let $text_display = $("<p>").text(question_text);
+            let info_text = val.category.title + " " + val.value;
+            let $question_info = $("<h3>").text(info_text);
+            $row.data("question", question_text);
+            $row.data("answer", answer_text);
+            $row.append($question_info)
+            $row.append($text_display);
+            $row.click(function() {
+                $text_display.text() == $row.data("answer") 
+                  ? $text_display.text($row.data("question")) 
+                  : $text_display.text($row.data("answer"));
+              });
             $table.append($row);
+            $table.append("<br>");
         });
-        $( "#result_table" ).remove();
-        $( document.body ).append($table);
+
     });
+
+    $( "#result_table" ).remove();
+    $( document.body ).append($table);
 
 }
 
