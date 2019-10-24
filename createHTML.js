8a3393e3aed68ebd9e4f87ec3ef315fb1e01041c
wwick <wwick@wustl.edu>
2019-10-23 23:41:50 -0500

    working prototype

--- createHTML.js
+++ createHTML.js
@@ -1,8 +1,11 @@
 function createTable() {
-    setCategory();
+    // create structure to hold clues
     let $table = $("<div>", {id:"result_table"});
     let url = "http://jservice.io/api/clues?";
+    
+    // only apply filters if boxes are checked
     if ($( "#category_box" ).is(':checked')) {
+        category_id = getCategory();
         url += `category=${category_id}&`;
     }
     if ($( "#value_box" ).is(':checked')) {
@@ -15,21 +18,35 @@ function createTable() {
 
     $.getJSON(url, function(data) {
         $.each( data, function( _key, val)  {
+            // filter HTML elements such as italics
             let question_text = val.question.replace(/<[^>]*>?/gm, '');
+            // if question has no text, skip it
+            if (question_text == "") {
+                return false;
+            }
             let answer_text = val.answer.replace(/<[^>]*>?/gm, '');
+            // create box
             let $row = $("<div>", {class:"clue", id:val.id});
+            // box initially displays question text
             let $text_display = $("<p>").text(question_text);
-            let info_text = val.category.title + " " + val.value;
+            let info_text = val.category.title.toTitleCase();
+            // gives clue value if clue value exists
+            if (val.value != null) {
+                info_text += " for " + val.value;
+            }
+            // category and usually value serves as box title
             let $question_info = $("<h3>").text(info_text);
+            // store both question and answer
             $row.data("question", question_text);
             $row.data("answer", answer_text);
             $row.append($question_info)
             $row.append($text_display);
+            // when box is clicked, swap between question and answer
             $row.click(function() {
                 $text_display.text() == $row.data("answer") 
                   ? $text_display.text($row.data("question")) 
                   : $text_display.text($row.data("answer"));
-              });
+            });
             $table.append($row);
             $table.append("<br>");
         });
@@ -39,5 +56,15 @@ function createTable() {
     $( "#result_table" ).remove();
     $( document.body ).append($table);
 
+}
+
+function getCategory() {
+    let query = $( "#category_field" ).val();
+    query = query.toLowerCase();
+    // wait for setup to fill dict with categories
+    if (dictFilled && query in dict) {
+        return dict[query];
+    }
+    return "";
 }
 
--- setup.js
+++ setup.js
@@ -1,17 +1,46 @@
+// global variables
 var home = true;
 var dict = {};
 var dictFilled = false;
-var category_id = null;
 var value = 600;
 var min_date = null;
 var max_date = null;
 
+// there are between 18400 and 18500 categories
 const MAX_OFFSET = 18400
+// iterate through 100 categories at a time
 for (let offset = 0; offset <= MAX_OFFSET; offset += 100) {
     $.getJSON( `http://jservice.io/api/categories?count=100&offset=${offset}`, function( data ) {
         $.each( data, function( _key, val ) {
+            // add categories to dictionary
             dict[val.title] = val.id;
         });
     });
 }
 dictFilled = true;
+
+// https://stackoverflow.com/questions/196972/convert-string-to-title-case-with-javascript
+String.prototype.toTitleCase = function() {
+    var i, j, str, lowers, uppers;
+    str = this.replace(/([^\W_]+[^\s-]*) */g, function(txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+  
+    // Certain minor words should be left lowercase unless 
+    // they are the first or last words in the string
+    lowers = ['A', 'An', 'The', 'And', 'But', 'Or', 'For', 'Nor', 'As', 'At', 
+    'By', 'For', 'From', 'In', 'Into', 'Near', 'Of', 'On', 'Onto', 'To', 'With'];
+    for (i = 0, j = lowers.length; i < j; i++)
+      str = str.replace(new RegExp('\\s' + lowers[i] + '\\s', 'g'), 
+        function(txt) {
+          return txt.toLowerCase();
+        });
+  
+    // Certain words such as initialisms or acronyms should be left uppercase
+    uppers = ['Id', 'Tv'];
+    for (i = 0, j = uppers.length; i < j; i++)
+      str = str.replace(new RegExp('\\b' + uppers[i] + '\\b', 'g'), 
+        uppers[i].toUpperCase());
+  
+    return str;
+}
